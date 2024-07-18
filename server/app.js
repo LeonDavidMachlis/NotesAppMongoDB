baee647f5615b74935c617e574afd4fe49c5fc09
gil weiss <138373913+nenene3@users.noreply.github.com>
2024-07-18 16:28:35 +0300

    2

--- server/app.js
+++ server/app.js
@@ -1,8 +1,10 @@
 import express from "express";
 import routerTasks from "./routers/tasks/tasksRouter.js";
+import morgan from "morgan";
 export const app = express();
 
 app.use(express.json());
+app.use(morgan('dev'));
 
 app.use("/api/tasks", routerTasks);
 
