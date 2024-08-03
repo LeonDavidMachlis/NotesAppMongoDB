ea244cba2f918b0ac3db33662b033d9514f78118
gil weiss <138373913+nenene3@users.noreply.github.com>
2024-08-03 11:21:46 +0300

    connected nodejs with front

--- server/routers/users/usersRouter.js
+++ server/routers/users/usersRouter.js
@@ -1,9 +1,12 @@
-import express from 'express'
-import * as usersControllers from '../../controllers/userContorllers.js'
+import express from "express";
+import * as usersControllers from "../../controllers/userContorllers.js";
 
 const router = express.Router();
 
 // router.route('/').get(tasksControllers.getTasks).post(tasksControllers.postTask)
-router.route('/:userId').get(usersControllers.getTasksByUser).delete(usersControllers.deleteUserTasks)
+router
+  .route("/:userId")
+  .get(usersControllers.getTasksByUser)
+  .delete(usersControllers.deleteUserTasks);
 
-export default router
+export default router;
